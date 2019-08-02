33509b525517b44cda69ff6846e81829bc48e07e
demx8as6 <martin.skorupski@highstreet-technologies.com>
2019-08-02 13:14:00 +0200

    Setup Seed repo for OAM.
    - parent root pom.xml created
    - several submodules added for Papyrus modelling
    - hello-world seed code
    
    [OAM-1]

---
+++ code/oam-aggregation/src/test/java/org/o-ran-sc/oam/root/AppTest.java
@@ -0,0 +1,20 @@
+package org.o_ran_sc.oam.root;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
